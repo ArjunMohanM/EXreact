a78a4cbc815360e655775b450381da328c6024fe
ArjunMohanM <arjunmohan054@gmail.com>
2022-07-26 16:11:05 +0530

    login page design done

--- transport_management/src/App.js
+++ transport_management/src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
-
+import img1 from './images/chechikutti.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section class="vh-100" style={{backgroundColor: 'red'}}>
+        <div class="container py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col col-xl-10">
+              <div class="card" style={{borderRadius: '5px'}}>
+                <div class="row g-0">
+                  <div class="col-md-6 col-lg-5 d-none d-md-block">
+                    <img src={img1}
+                      alt="login form" class="img-fluid" style={{border: "1px solid black"}} />
+                  </div>
+                  <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                    <div class="card-body p-4 p-lg-5 text-black">
+
+                      <form>
+
+                        <div class="d-flex align-items-center mb-3 pb-1">
+                          <i class="fas fa-cubes fa-2x me-3" style={{color: 'white'}}></i>
+                          <span class="h1 fw-bold mb-0">Logo</span>
+                        </div>
+
+                        <h5 class="fw-normal mb-3 pb-3" style={{letterSpacing : '1px'}}>Sign into your account</h5>
+
+                        <div class="form-outline mb-4">
+                          <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                          <label class="form-label" for="form2Example17">Email address</label>
+                        </div>
+
+                        <div class="form-outline mb-4">
+                          <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                          <label class="form-label" for="form2Example27">Password</label>
+                        </div>
+
+                        <div class="pt-1 mb-4">
+                          <button class="btn btn-dark btn-lg btn-block" type="button">Login</button>
+                        </div>
+
+                        <a class="small text-muted" href="#!">Forgot password?</a>
+                        <p class="mb-5 pb-lg-2" style={{colors: 'blue'}}>Don't have an account? <a href="#!"
+                          style={{colors: 'blue'}}>Register here</a></p>
+                        <a href="#!" class="small text-muted">Terms of use.</a>
+                        <a href="#!" class="small text-muted">Privacy policy</a>
+                      </form>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
